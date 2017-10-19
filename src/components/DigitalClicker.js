db754fc36c81381ed89d8b7bf14a0df79a657675
will-sauer-learn <will@heueso.com>
2017-10-19 11:07:30 -0400

    done

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+export default class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  clicked = () => {
+    this.setState({
+      timesClicked: this.state.timesClicked+=1
+    })
+  }
+
+  render(){
+    return <button onClick={this.clicked}>{this.state.timesClicked}</button>
+  }
+}
+
+
+
+
+// The component renders out a button with a label that shows the timesClicked value.
+// This means that, at the start, your button should just say 0.
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,43 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component{
+  constructor (){
+    super()
+    this.state = (
+    {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    })
+  }
+
+  changeBitRate = () => {
+    this.setState({
+      settings: {...this.state.settings, bitrate: 12}
+    })
+  }
+
+  changeResolution = () => {
+    var video = {resolution: '720p'}
+    this.setState({
+      settings: {...this.state.settings, video}
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.changeBitRate}>{this.state.settings.bitrate}</button>
+        <button className="resolution" onClick={this.changeResolution}>{this.state.settings.video.resolution}</button>
+      </div>
+    )
+  }
+}
+
+
+// Create a button with the class 'resolution'. Clicking this button changes the settings.video.resolution state property to '720p'.
