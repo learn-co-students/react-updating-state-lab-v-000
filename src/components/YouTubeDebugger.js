6ce4cfd08bdeab9dd8b6a48186e6255038c6d3d2
Hannah Carnes <hecarnes@gmail.com>
2018-03-16 16:31:14 -0400

    digitalClicker works

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,28 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  handleClick() {
+    // Update our state here...
+    this.setState(({ count: this.state.timesClicked + 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
+
+export default YouTubeDebugger;
+
