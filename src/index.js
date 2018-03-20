75cd4ef47efa1a315077379e4d189e2223ff9f4e
PRbsas <paularamirez0316@gmail.com>
2018-03-20 10:08:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import DigitalClicker from './components/DigitalClicker';
-import YouTubeDebugger from './components/YouTubeDebugger';
+import DigitalClicker from './components/DigitalClicker'
+import YouTubeDebugger from './components/YouTubeDebugger'
 
 ReactDOM.render(
   <div>
@@ -10,4 +10,4 @@ ReactDOM.render(
     <YouTubeDebugger />
   </div>,
   document.getElementById('global')
-);
+)
