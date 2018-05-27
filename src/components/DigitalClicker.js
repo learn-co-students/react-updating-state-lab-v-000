c8285db9458a4400761f5b4c7a20a3638daf5449
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-27 15:31:15 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,65 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      timesClicked: ++this.state.timesClicked,
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    );
+  }
+}
+
+export default DigitalClicker;
+
+
+// // Code DigitalClicker Component Here
+//
+//
+// import React from 'react'
+//
+// class DigitalClicker extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       timesClicked: 0,
+//       // label: "0"
+//     }
+//
+//   }
+//
+//   handleClick = () => {
+//
+//       // this is from FI solution but won't it lead to compounding.  When
+//       //timesClicked is two, then the number will go up by two.
+//       timesClicked: ++this.state.timesClicked
+//
+//       // this.setState((timesClicked) => {
+//       // return {timesClicked +1}})
+//       // {
+//       // timesClicked += 1
+//       // }
+//       // }
+//     }
+//
+//   render() {
+//     return(
+//       <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+//       // <button label= {this.state.label} onClick={this.handleClick} />
+//     )
+//   }
+// }
+//
+// export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,83 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: { resolution: '1080p' }
+
+      }
+    }
+  }
+
+  handleBitrate = () => {
+    this.setState({
+        settings: {
+          ...this.state.settings,
+          bitrate: 12
+      }
+    })
+  }
+
+  handleResolution = () => {
+    // this.setState({
+    //         settings: {
+    //           ...this.state.settings,
+    //           video: {
+    //             ...this.state.settings.video,
+    //             resolution: '720p'
+    //           }
+    //         },
+    //       });
+
+
+    this.setState({
+            settings: {
+              ...this.state.settings,
+              video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+              }
+            },
+          });
+        }
+
+    // this.setState({
+    //     settings: {
+    //       ...this.state.settings,
+    //       video:
+    //       {...this.state.settings.video,
+    //       resolution: '720p'}
+    //   }
+    // })}
+
+    // not work:
+    // this.setState({
+    //     settings: {
+    //       video: {
+    //         resolution: '720p'
+    //     }
+    //   }
+    // })
+  // }
+
+  render() {
+    return (
+
+      <div>
+        <button className="bitrate" onClick={this.handleBitrate}>Bitrate</button>
+
+        <button className="resolution" onClick={this.handleResolution}>Resolution</button>
+      </div>
+    )
+  }
+
+}
+
+export default YouTubeDebugger
