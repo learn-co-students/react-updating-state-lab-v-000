20cbc65d65e73633a92d14aafd874bf814f6b74c
jendobes <jdobak1@gmail.com>
2018-06-05 15:15:03 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleOnClick = () => {
+    this.setState({timesClicked: ++this.state.timesClicked})
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleOnClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p' }
+      }
+    }
+  }
+
+  handleBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: "720p"
+        }
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div>
+      <button className='bitrate' onClick={this.handleBitrate}></button> //changes the settings.bitrate state property to 12.
+      <button className='resolution' onClick={this.handleResolution}></button> //changes the settings.video.resolution state property to '720p'
+      </div>
+    )
+  }
+}
