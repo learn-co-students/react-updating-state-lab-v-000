79fff55fef9fad3a4ca26112587c3f9efc0267e2
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-07-06 17:51:04 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    // Update our state here...
+      this.setState({
+     timesClicked: this.state.timesClicked + 1
+   })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I have been clicked {this.state.timesClicked} times!</p>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    );
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+           errors: [],
+           user: null,
+           settings: {
+             bitrate: 8,
+             video: {
+               resolution: '1080p'
+             }
+           }
+    };
+  }
+
+  changeBitrate = () => {
+    // Update our state here...
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12,
+      },
+    });
+  }
+
+  changeResolution = () => {
+    // Update our state here...
+    this.setState({
+      settings: {
+          ...this.state.settings,
+          video: {
+            ...this.state.settings.video,
+            resolution: '720p'
+          }
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button className='bitrate' onClick={this.changeBitrate}>Increase bitrate</button>
+        <button className='resolution' onClick={this.changeResolution}>Change resolution</button>
+      </div>
+    );
+  }
+}
+
+export default YouTubeDebugger;
