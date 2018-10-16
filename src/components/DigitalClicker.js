0415045e5b01555664788c30eb4bceac0751dba2
peterckim <peterckim96@gmail.com>
2018-10-16 14:59:47 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
-// Code DigitalClicker Component Here
+// Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  incrementClicked = () => {
+    this.setState({ timesClicked: this.state.timesClicked + 1 });
+  };
+
+  render() {
+    return (
+      <button onClick={this.incrementClicked}>{this.state.timesClicked}</button>
+    );
+  }
+}
