fe882713954fdf113e25a77706f2300431c0337d
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-10-23 22:38:26 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  handleClikc = () => {
+    timeClicked + 1;
+  }
+  render(){
+    return(
+      <button onClick={this.handleClick}>
+        {this.timeCliked}
+      </button>
+
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,42 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+export default class YouTubeDebugger extends React.Componetn{
+  constructor(){
+    super()
+    this.state = {
+      js { errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } }}
+
+    }
+  }
+  handleBitrate = () => (
+    this.setState({
+      js: {
+      settings: {
+      ...this.state.settings,
+      bitrate: 12
+    }
+  }
+    })
+  )
+  handleResoluton = () => (
+    this.setState({
+      js: {
+        settings: {
+          video: {
+            ...this.state.video,
+            resoluton: '720p'
+
+          }
+        }
+      }
+    })
+  )
+
+  render(){
+    return(
+      <button className="bitrate" onClick={this.handleBitrate}></button>
+      <button className="resoluton" onClick-{this.handleResoluton}></button>
+
+    )
+  }
+}
