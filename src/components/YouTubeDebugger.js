ead5c5c01ea78a6288436f5894b6d28785b35cc4
smithwebtek <brad@smithwebtek.com>
2018-11-18 20:15:41 -0500

    Update lab

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,58 +1,57 @@
 import React, { Component } from 'react';
 
 class YouTubeDebugger extends Component {
-  constructor(){
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      errors: [],
-      user: null,
-      settings: {
-        bitrate: 8,
-        video: {
-          resolution: "1080p"
-        }
-      }
-    }
-  }
-  
-  handleClickedBitrate = () => {
+		this.state = {
+			errors: [],
+			user: null,
+			settings: {
+				bitrate: 8,
+				video: {
+					resolution: "1080p"
+				}
+			}
+		}
+	}
 
-    this.setState({
-      errors: [],
-      user: null,
-      settings: {
-        bitrate: 12,
-        video: {
-          resolution: "1080p"
-        }
-      }
-    })
-    console.log(this.state.settings.bitrate)
-  }
-  
-  handleClickedResolution = () => {
-    this.setState({
-      errors: [],
-      user: null,
-      settings: {
-        bitrate: 8,
-        video: {
-          resolution: "720p"
-        }
-      }
-    })
-    console.log(this.state.settings.video.resolution)
-  };
+	handleClickedBitrate = () => {
+		this.setState({
+			errors: [],
+			user: null,
+			settings: {
+				bitrate: 12,
+				video: {
+					resolution: "1080p"
+				}
+			}
+		})
+		console.log(this.state.settings.bitrate)
+	}
 
-  render() {
-    return (
-      <div>
-        <button className='bitrate' onClick={ this.handleClickedBitrate }>Bitrate</button>
-        <button className='resolution' onClick={ this.handleClickedResolution }>Resolution</button>
-      </div>
-    )
-  }
+	handleClickedResolution = () => {
+		this.setState({
+			errors: [],
+			user: null,
+			settings: {
+				bitrate: 8,
+				video: {
+					resolution: "720p"
+				}
+			}
+		})
+		console.log(this.state.settings.video.resolution)
+	};
+
+	render() {
+		return (
+			<div>
+				<button className='bitrate' onClick={this.handleClickedBitrate}>Bitrate</button>
+				<button className='resolution' onClick={this.handleClickedResolution}>Resolution</button>
+			</div>
+		)
+	}
 };
 
 export default YouTubeDebugger;
