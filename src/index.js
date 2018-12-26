9744ef8129494be90855fc067b23a1ef5d92afee
github username <github email address>
2018-12-26 12:29:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import YouTubeDebugger from './components/YouTubeDebugger';
 
 ReactDOM.render(
   <div>
-    <DigitalClicker />
-    <YouTubeDebugger />
-  </div>,
+    <DigitalClicker />,
+     <YouTubeDebugger />
+   </div>,
   document.getElementById('global')
 );
