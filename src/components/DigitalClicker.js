eb30a352f504ae2d4178299b4922005aa2f0b0d9
github username <github email address>
2019-01-21 13:48:44 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+  constructor() {
+  	super();
+
+  	this.state = {
+  	  timesClicked: 0
+  	};
+  }
+
+  handleClick = () => {
+  	this.setState({
+  	  timesClicked: this.state.timesClicked + 1
+  	}, () => console.log(this.state.timesClicked))
+  }
+
+  render() {
+  	return (
+  	  <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+  	);
+  }
+}
+
+{/*
+// In the components/DigitalClicker.js file, create a DigitalClicker React component.
+// This component has an initial state property called timesClicked, which is initially defined as 0.
+// The component renders out a button with a label that shows the timesClicked value. This means that, at the start, your button should just say 0.
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
+*/}
