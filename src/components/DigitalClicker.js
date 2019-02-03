15a3c5b2cd10d10cf31ba2f0e6c711da50767bd2
Vibraniumforge <gregm9999@gmail.com>
2019-02-03 15:48:45 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+class DigitalClicker extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () =>{
+        const timesClicked=this.state.timesClicked;
+        this.setState({timesClicked: timesClicked +1});
+    }
+
+    render() {
+        return (
+            <div><button type="button" id="button" onClick={this.handleClick}>{this.state.timesClicked}</button></div>
+        )
+    }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,37 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+class YouTubeDebugger extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+                errors:[],
+                user: null,
+                settings: { 
+                    bitrate: 8, 
+                    video: { resolution: '1080p' } 
+                } 
+        }
+    }
+
+    handleBitrate = () =>{
+        this.setState({settings: Object.assign({}, this.state.settings, {
+            bitrate: 12
+            })
+        });
+    }
+
+    handleResolution = () => {
+        this.setState({settings:{...this.state.settings, video: {...this.state.settings.video, resolution: '720p'}}})
+    }
+
+    render() {
+        return (
+            <div><button type="button" className="bitrate" onClick={this.handleBitrate}>Change Bitrate</button>
+            <button type="button" className="resolution" onClick={this.handleResolution}>Change Resolution</button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger;
