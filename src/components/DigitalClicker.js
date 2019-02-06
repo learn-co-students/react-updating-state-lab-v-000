e05d68fcffe18d07acef73697ff4b3b85c3d39d3
Kachel Wittig <kjwittig@gmail.com>
2019-02-06 12:39:28 -0600

    Donezo

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0,
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      // incremental operator (++) works directly against the state value
+      // stackoverflow.com/questions/39316376/how-to-use-the-increment-operator-in-react
+      timesClicked: this.state.timesClicked + 1,
+    })
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+  }
+}
+
+export default DigitalClicker
