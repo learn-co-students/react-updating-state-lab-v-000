9bbc36bceae83eadfa768f8e3c70fb967c693b02
Madeline-Stark <madeline.stark1@gmail.com>
2019-02-22 13:13:15 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  updateTimesClicked = () => {
+    this.setState((prevState) => ({ //can't access this inside of set state so need to access previous state
+        timesClicked: prevState.timesClicked +1
+    }))
+  };
+
+  render() {
+    return (
+      <button
+      onClick={this.updateTimesClicked}
+      >
+      {this.state.timesClicked}
+      </button>
+    );
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,63 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+
+  updateBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings, //includes all other previous parts of array, just updates bitrate
+        bitrate: 12
+      }
+    });
+  };
+
+  updateResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings, //includes all other previous parts of array, just updates bitrate
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div> //both buttons need to be inside of div or else won't render right
+      <button
+      className="bitrate"
+      onClick={this.updateBitrate}
+      >
+      Update Bitrate
+      </button>
+
+      <button
+      className="resolution"
+      onClick={this.updateResolution}
+      >
+      Update Resolution
+      </button>
+      </div>
+    );
+  }
+}
+
+
+export default YouTubeDebugger;
