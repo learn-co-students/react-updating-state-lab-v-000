fcfa348ec92bd092c4e77a7912dd619d4037aa72
epic0448 <samuelogundiran@Yahoo.com>
2019-04-06 17:52:12 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class  DigitalClicker  extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            timesClicked: 0
+        };
+    }
+
+        handleClick = () => {
+
+            this.setState(prevState => ({
+                timesClicked: prevState.timesClicked +1
+            })) 
+        }
+
+        render() {
+            return (
+                <div>
+                <p> {this.state.timesClicked ? this.setState : null} </p>
+                <button onClick={this.handleClick}> 1 </button>
+                </div>
+            )
+         }
+    }
+
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,28 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+             errors: [], 
+             user: null, 
+             settings: { bitrate: 8, video: { resolution: '1080p' }} 
+            
+        }
+    }  
+    render() {
+        return (
+            <div>
+            <p> {this.bitrate ? 12 : null} </p>
+            <button onClick={this.bitrate}>Bitrate</button>
+          
+            <p> {this.resolution ? '720p' : null} </p>
+            <button onClick={this.resolution}>Resolution</button>
+            </div>
+        )
+     }
+}
+
+export default YouTubeDebugger;
