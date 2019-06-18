3e21a3751f6f205e178646cdd81b1b20bc090d4c
indykiss <indirak@alumni.princeton.edu>
2019-06-18 12:52:19 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            timesClicked: 0
+        };
+    }
+
+    handleClick = () => {
+        this.setState({
+            timesClicked: this.state.timesClicked + 1      
+        })
+    }
+
+    render() {
+        const count = this.state.timesClicked                
+
+        return (
+            <div>
+                <button onClick= {this.handleClick}>{count}</button>
+            </div>
+        )
+    }
+
+
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+       }
+    }
+
+    newBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                    bitrate: 12
+            }
+        })
+    }
+      
+
+    newResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <button className = "bitrate" onClick={this.newBitrate}>Change Bitrate</button>
+                <button className = "resolution" onClick={this.newResolution}>Change Resolution</button>
+            </div>
+        )
+    }
+
+
+}
