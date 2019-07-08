2e011e4bb36dbf1c89b4b2aa5c4c3c358180e195
github username <github email address>
2019-07-08 13:15:01 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+  super()
+
+  this.state = {
+    timesClicked: 0
+  };
+}
+
+handleClick = () => {
+  let a = 1
+  this.setState({
+    timesClicked: ++a
+  })
+}
+
+  render(){
+    return(
+
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    )
+  }
+
+}
+
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,13 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+  render() {
+    return (
+      <div>Hi</div>
+    )
+  }
+}
+
+
+export default YouTubeDebugger;
