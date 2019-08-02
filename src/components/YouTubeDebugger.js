66406c4c43e93cf2c2514d389cad49d7af2298fc
Ashley <ashleywong678@gmail.com>
2019-08-02 10:56:12 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,26 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React from 'react';
+
+ class YouTubeDebuggerß extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+    }
+  }
+    }
+  }
+
+  render(){
+    return(
+      <button class='bitrate'></button>
+    )
+  }
+ }
+ export default YouTubeDebuggerß;
