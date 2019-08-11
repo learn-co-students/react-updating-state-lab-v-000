00e33c4080b98aa21b93555e7f2333f99e33b3f2
Christopher DiStasio <cadistasio@gmail.com>
2019-08-11 12:20:01 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+// import { timingSafeEqual } from 'crypto';
+
+export default class DigitalClicker extends React.Component {
+
+	constructor(){
+		super()
+		this.state = {
+			timesClicked: 0
+		}
+	}
+	
+	updateTimer = () => {
+		this.setState({
+			timesClicked: this.state.timesClicked + 1
+		})
+	}
+	
+	render(){
+		return (
+			<div>
+				<button onClick={this.updateTimer}>
+					{this.state.timesClicked}
+				</button>
+				<br></br>
+				<br></br>
+			</div>
+		)
+ 	}   
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,10 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+
+	render(){
+		return (
+			'hello'
+		)
+	 }   			
+}
