0e6b261af73471aa6a76ad5d53df7fe1f8b5650b
Rafael Lopez <lpz.raf@gmail.com>
2019-08-24 15:45:40 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,12 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
