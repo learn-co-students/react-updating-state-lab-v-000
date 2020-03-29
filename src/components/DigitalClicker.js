aa9855a718983941891aef4dce66a1bfbf9378ff
Rebecca Tong <rtong@buffalo.edu>
2020-03-29 14:09:15 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  clickity = () => {
+    let newCount = this.state.timesClicked + 1
+    this.setState({
+      timesClicked: newCount
+    })
+  }
+  render(){
+    return(
+    <div>
+    <button
+    onClick={this.clickity}>{this.state.timesClicked}</button>
+    </div>
+  )
+    }
+}
