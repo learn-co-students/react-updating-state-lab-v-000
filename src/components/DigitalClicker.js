52b8cf82b8d4b059d0590f179e5c716000f0ca58
iamtamtamiam <vutam_a@hotmail.com>
2020-07-01 16:32:18 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,42 @@
 // Code DigitalClicker Component Here
+
+
+import React, {Component} from 'react';
+ 
+export default class DigitalClicker extends Component {
+ 
+    constructor(){
+        super()
+        this.state = {
+            timesClicked : 0
+        }
+
+    }
+
+
+    addClick = () => {
+      console.log('Tee hee!');
+
+      // let newCount = this.state.count + 1
+      // this.setState({
+      //   count: newCount
+      // })
+
+      this.setState(previousState => {
+        return {
+          timesClicked: previousState.timesClicked + 1
+        }
+      })
+
+
+    }
+   
+    render() {
+      return (
+        <div>
+            
+            <button onClick={this.addClick}>{this.state.timesClicked}</button>
+        </div>
+      );
+    }
+}
