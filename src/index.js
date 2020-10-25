8abfbd9478ae9bb4a36769e923836fe08f65df7d
RonSala <ron.sala@gmail.com>
2020-10-25 12:58:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DigitalClicker from './components/DigitalClicker';
-// import YouTubeDebugger from './components/YouTubeDebugger';
+import YouTubeDebugger from './components/YouTubeDebugger';
 
 ReactDOM.render(
   <div>
     <DigitalClicker />
-    {/* <YouTubeDebugger /> */}
+    <YouTubeDebugger />
   </div>,
   document.getElementById('global')
 );
