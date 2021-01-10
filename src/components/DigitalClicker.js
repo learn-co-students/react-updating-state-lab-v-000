e5f502227a07b2eadd318c36725332bf91800307
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-10 18:30:16 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+    class DigitalClicker extends Component { 
+        
+        constructor() {
+            super();
+
+            this.state = {timesClicked: 0}
+        }
+
+        handleClicker = () => {
+            this.setState(prevState => ({timesClicked: prevState.timesClicked+1}))
+        }
+
+        render() {
+            return(
+                <div>
+                    <button className="click" onClick={this.handleClicker}>{this.state.timesClicked}</button>
+                </div>
+            )
+        }
+
+    }
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,67 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+    class YouTubeDebugger extends Component { 
+        
+        constructor() {
+            super();
+
+            this.state = {
+                errors: [],
+                user: null,
+                settings: {
+                    bitrate: 8,
+                    video: {
+                        resolution: '1080p'
+                    }
+                } 
+            };
+        }
+
+        handleChangeBit = () => {
+            this.setState({
+                settings: {
+                    ...this.state.settings,
+                    bitrate: 12
+                }
+            });
+        };
+
+        handleChangeRes = () => {
+            this.setState({
+                settings: {
+                    ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                resolution: '720p'
+                }
+            }
+        });
+        }
+
+        /*
+    Or this can be defined using the Object.assign method:
+    
+        this.setState({
+            settings: Object.assign({}, this.state.settings, {
+                video: Object.assign({}, this.state.settings.video, {
+                resolution: '720p'
+                })
+            })
+        });
+        */
+
+
+        render() {
+            return(
+                <div>
+                    <button className="bitrate" onClick={this.handleChangeBit}>Change Bitrate</button>
+                    <button className="resolution" onClick={this.handleChangeRes}>Change Resolution</button>
+                </div>
+            )
+        }
+
+    }
+
+export default YouTubeDebugger;
