6baa443b68979dea930d0d9e97245a29de15684a
ricecake88 <gracelearnsforever@gmail.com>
2021-03-14 18:03:19 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react';
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleButton = (event) => {
+        this.setState(previousState => {
+            return {timesClicked: previousState.timesClicked + 1}
+        })
+    }
+
+    render() {
+        return <div><button onClick={this.handleButton} name={this.state.timesClicked}>{this.state.timesClicked}</button></div>
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }         
+    }
+
+    handleBitrate = (event) => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    handleResolution = (event) => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleBitrate}>Change Bitrate</button>
+                <button className="resolution" onClick={this.handleResolution}>Change Resolution</button>
+            </div>)
+    
+
+    }
+}
