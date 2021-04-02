6776b0804377821481a2172216283e03655c084d
Damien de Lartigue <damien@driving.vision>
2021-04-02 16:32:21 +0100

    all tests passing

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+	constructor(props) {
+		super()
+
+		this.state = {
+			timesClicked: 0
+		}
+	}
+
+	clickHandler = () => {
+		this.setState(previousState => {
+			return {
+				timesClicked: 1 + previousState.timesClicked
+			}
+		})
+	}
+
+	render(){
+		return(
+			<button onClick={this.clickHandler}>{this.state.timesClicked}</button>
+			)
+	}
+}
